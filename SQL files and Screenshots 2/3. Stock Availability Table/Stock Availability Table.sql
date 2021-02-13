e18db1e8e3c0083dd8cc00de04749c962d4005d3
Aman24711 <47086643+Aman24711@users.noreply.github.com>
2021-02-13 12:53:39 +0530

    Add files via upload

---
+++ SQL files and Screenshots 2/3. Stock Availability Table/Stock Availability Table.sql
@@ -0,0 +1,9 @@
+/* Creating Table Stock Availability */
+CREATE TABLE IF NOT EXISTS StockAvailability(
+  StkId INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Prod_Id INT(11), 
+  StkAvail INT(11), 
+  Price DECIMAL(10, 2),  
+  Date_ DATE
+);
+
